e75f1ab425285178e76b327dc4ee25a7981a8ea6
David Pereira <david.pereira@onofre.com.br>
2018-08-01 17:02:45 -0300

    Lidando com a entrada do usuario

--- advinhacao.py
+++ advinhacao.py
@@ -1,1 +1,17 @@
+print("********************************")
 print("Bem vindo ao jogo de advinhacao!")
+print("********************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu numero: ")
+
+print("Você digitou ", chute_str)
+
+chute = int(chute_str)
+
+if numero_secreto == chute:
+    print("Você acertou")
+else:
+    print("Você errou")
+
