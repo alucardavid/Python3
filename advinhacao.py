29b750a6f479f3225e1c4c35015c0b223010f0db
David Pereira <d_pereira@outlook.com.br>
2018-08-01 23:50:47 -0300

    Testando valores

--- advinhacao.py
+++ advinhacao.py
@@ -9,9 +9,16 @@ chute_str = input("Digite o seu numero: ")
 print("Você digitou ", chute_str)
 
 chute = int(chute_str)
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
 
-if numero_secreto == chute:
+if acertou:
     print("Você acertou")
 else:
-    print("Você errou")
+    if maior:
+        print("Você errou! O seu chute foi maior do que número secreto.")
+    elif menor:
+        print("Você errou! O seu chute foi menor do que número secreto.")
 
+print("Fim do jogo")
