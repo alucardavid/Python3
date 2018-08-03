8452b7f5924c5ceb10f2454bb1ae67235c7c1d9e
David Pereira <david.pereira@onofre.com.br>
2018-08-03 11:40:50 -0300

    Utilizando laço for

--- advinhacao.py
+++ advinhacao.py
@@ -6,7 +6,7 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while (rodada <= total_de_tentativas):
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite o seu numero: ")
     print("Você digitou ", chute_str)
@@ -23,7 +23,5 @@ while (rodada <= total_de_tentativas):
             print("Você errou! O seu chute foi maior do que número secreto.")
         elif (menor):
             print("Você errou! O seu chute foi menor do que número secreto.")
-
-    rodada = rodada + 1
 
 print("Fim do jogo")
