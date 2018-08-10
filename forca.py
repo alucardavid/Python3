2f92f6040345e706b9d04c76ade26d585a211324
David Pereira <d_pereira@outlook.com.br>
2018-08-09 23:07:00 -0300

    Iniciando jogo forca

--- forca.py
+++ forca.py
@@ -4,6 +4,15 @@ def jogar():
     print("Bem vindo ao jogo da forca!")
     print("********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando....")
+
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
